6da2a897c699cb4999b516b7b158626f2135653f
TheLover_Z <sethverlo5@gmail.com>
2014-01-17 17:03:20 +0800

    add regex support to ruby-2-3.

--- ruby-2-3.rb
+++ ruby-2-3.rb
@@ -3,8 +3,12 @@
 def grep(pattern, filename)
   f = open(filename, 'r')
   f.each_with_index do |line, index|
-    puts "line #{index + 1}: #{line}" if line.index(pattern)
+    if line.index(pattern) || line.index(Regexp.new pattern)
+      puts "line #{index + 1}: #{line}" 
+    end
   end
 end
 
 grep('God', 'aFile')
+puts
+grep('\bon\b', 'aFile')
