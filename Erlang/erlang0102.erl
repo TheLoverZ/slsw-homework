434622df84a29b41b2b32c6fe7e7682390099470
TheLover_Z <sethverlo5@gmail.com>
2014-01-28 21:33:24 +0800

    erlang-1-2 added.

---
+++ Erlang/erlang0102.erl
@@ -0,0 +1,13 @@
+% 写一个递归计数到 10 的函数。
+
+-module(erlang0102).
+-export([recurCount/1]).
+
+recurCount(10) -> 10;
+recurCount(N) -> recurCount(N + 1).
+
+% 输出
+% 11> c(erlang0102).
+% {ok,erlang0102}
+% 12> erlang0102:recurCount(1).
+% 10
